9ea70ce083a78bdb595d5ad040687d4eccf312e7
Victor Untulius <untulius@gmail.com>
2020-03-09 21:13:52 +0300

    add new line

--- src/com/github/gittest/Hello.java
+++ src/com/github/gittest/Hello.java
@@ -3,5 +3,6 @@ package com.github.gittest;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        System.out.println("new string in master branch");
     }
 }
